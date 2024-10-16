b059e1d6d349ad755bec18e8ea933d271a0e6bdb
TamiBeira <tamirisbeira@gmail.com>
2024-10-16 12:04:03 -0300

    corrigindo importação saga

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,6 +1,6 @@
-import { all } from "redux-saga";
+import { all } from "redux-saga/effects";
 import user from "../redux/user/saga";
 
 export default function* rootSaga() {
-    return yield all([user]);
+    yield all([user()]);
 }
